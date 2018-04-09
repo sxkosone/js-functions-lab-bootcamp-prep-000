6317ce1583b6745636213dd1441212016a91b725
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-09 15:40:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidayTo(holiday, name) {
   return "Happy " + holiday +", " + name + "!";
 }
 
-function holidayCountdown(holiday, day) {
+function holidayCountdown(holiday, days) {
   return "It's "+ days +" days until " + holiday+"!";
 }
 
